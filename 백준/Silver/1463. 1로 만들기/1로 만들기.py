0781bd4615bcfb18b701225baa0cc15f0dc6deca
seoyounghan <63441374+seoyounghan@users.noreply.github.com>
2023-03-24 20:37:06 +0900

    [Silver III] Title: 1로 만들기, Time: 52 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,22 @@
 N = int(input())
 
-li = [0]*(N+1) 
+li = [N+1]*(N+1) 
 
-for i in range(2,N+1):
+queue = [N]
+li[N] = 0
 
-	li[i] = li[i-1] + 1
+while li[1] == N+1:
+	curr = queue.pop(0)
+	curr_count = li[curr]
 
-	if i % 3 == 0:
-		li[i] = min(li[i],li[i//3]+1)
-	if i % 2 == 0:
-		li[i] = min(li[i],li[i//2]+1)
-print(li[N])
+	if curr % 3 == 0 and li[curr//3] == N+1:
+		queue.append(curr // 3)
+		li[curr//3] = curr_count + 1
+	if curr % 2 == 0 and li[curr//2] == N+1:
+		queue.append(curr // 2)
+		li[curr//2] = curr_count + 1
+	if li[curr-1] == N+1:
+		queue.append(curr-1)
+		li[curr-1] = curr_count + 1
+
+print(li[1])
