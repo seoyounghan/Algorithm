7ab12928daac8485b9d69fbb6ae8a406b38199d3
seoyounghan <63441374+seoyounghan@users.noreply.github.com>
2023-07-07 16:29:07 +0900

    [unrated] Title: 배열 만들기 4, Time: 4.36 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ arr:[Int]) -> [Int] {
+    var stk: [Int] = []
+    var i: Int = 0
+    
+    while i < arr.count {
+        if stk.isEmpty {
+            stk.append(arr[i])
+            i += 1
+        } else {
+            if (stk.last)! < arr[i] {
+                stk.append(arr[i])
+                i += 1
+            } else {
+                stk.popLast()
+            }
+        }
+    }
+    return stk
+}
