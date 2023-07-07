0b526afbe51ae4b7a98f994163e838cef84dcd8c
seoyounghan <63441374+seoyounghan@users.noreply.github.com>
2023-07-07 16:20:46 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ n:Int) -> [Int] {
+    var ans: [Int] = [n]
+    var x: Int = n
+
+    while x != 1 {
+        if x%2 == 0 {
+            x = x / 2
+        } else {
+            x = 3 * x + 1
+        }
+        ans.append(x)
+    }
+    return ans
+}
