9596c311100f9aed5099373f1d7e73b30078a8fe
seoyounghan <63441374+seoyounghan@users.noreply.github.com>
2024-04-05 13:59:45 +0900

    [Silver I] Title: IOIOI, Time: 356 ms, Memory: 32368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,20 +3,21 @@ input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-S = list(input())
-
-findStr = "I"
+S = input()
 ans = 0
+i = 0
+count = 0
 
-for i in range(N):
-	findStr += "OI"
+while i < (M - 1):
+    if S[i:i+3] == "IOI":
+        i += 2
+        count += 1
+        if count == N:
+            ans += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
 
-StrLen = len(findStr)
-
-for i in range(M-StrLen+1):
-	if list(findStr) == S[i:i+StrLen]:
-		ans+= 1
-	#print(S[i:i+StrLen])
-#print(findStr)
 print(ans)
 
