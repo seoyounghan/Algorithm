c88ee6cde47e3a8a92370bce3c30ffc00b019f8e
seoyounghan <63441374+seoyounghan@users.noreply.github.com>
2024-05-10 16:49:05 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+li = []
+
+for i in range(N):
+	li.append(list(input().split()))
+
+class Node():
+    def __init__(self, item, left, right):
+        self.item = item
+        self.left = left
+        self.right = right
+
+def preorder(node):
+    print(node.item, end = '')
+    if node.left != '.':
+        preorder(tree[node.left])
+    if node.right != '.':
+        preorder(tree[node.right])
+        
+def inorder(node):
+    if node.left != '.':
+        inorder(tree[node.left])
+    print(node.item, end = '')
+    if node.right != '.':
+        inorder(tree[node.right])
+        
+def postorder(node):
+    if node.left != '.':
+        postorder(tree[node.left])
+    if node.right != '.':
+        postorder(tree[node.right])
+    print(node.item, end = '')
+
+tree = {}
+
+for item, left, right in li:
+    tree[item] = Node(item, left, right)
+
+preorder(tree['A'])
+print()
+inorder(tree['A'])
+print()
+postorder(tree['A'])
