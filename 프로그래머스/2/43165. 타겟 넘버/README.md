84f97172cec336d4ff37c2505e1ad0d00a390d16
seoyounghan <63441374+seoyounghan@users.noreply.github.com>
2025-03-17 09:15:58 +0900

    [level 2] Title: 타겟 넘버, Time: 41.86 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 16 MB, 시간: 27.84 ms
+메모리: 16.4 MB, 시간: 41.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 13일 19:54:20
+2025년 03월 17일 09:15:56
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -2,19 +2,22 @@ import Foundation
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     var ans = 0
+    var numbersCount = numbers.count
     
-    func findNum(_ sum: Int, _ index: Int) {
-        if index == numbers.count - 1 {
-            if (sum + numbers[index]) == target || (sum - numbers[index]) == target {
+    func dfs(index: Int, num: Int) {
+        if index == numbersCount - 1 {
+            if (num + numbers[index] == target) || (num - numbers[index] == target) {
                 ans += 1
             }
         } else {
-            findNum(sum + numbers[index], index + 1)
-            findNum(sum - numbers[index], index + 1)
+            dfs(index: index + 1, num: num + numbers[index])
+            dfs(index: index + 1, num: num - numbers[index])
         }
+        
     }
     
-    findNum(0, 0)
+    dfs(index: 0, num: 0)
+    
     
     return ans
 }
